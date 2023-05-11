cb3ad8f86d97c1db8ecfdf807f5c570733ed28c6
Brandin Canfield <bcanfield@miletwo.us>
2023-05-10 20:11:36 -0400

    Initial CLI Setup

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+const { Command } = require("commander");
+const figlet = require("figlet");
+const fs = require("fs");
+const path = require("path");
+
+async function listDirContents(filepath: string) {
+  try {
+    const files = await fs.promises.readdir(filepath);
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+      const { size, birthtime } = fileDetails;
+      return { filename: file, "size(KB)": size, created_at: birthtime };
+    });
+    const detailedFiles = await Promise.all(detailedFilesPromises);
+    console.table(detailedFiles);
+  } catch (error) {
+    console.error("Error occurred while reading the directory!", error);
+  }
+}
+const program = new Command();
+
+console.log(figlet.textSync("Prisnext"));
+program
+  .version("1.0.0")
+  .description("An example CLI for managing a directory")
+  .option("-l, --ls  [value]", "List directory contents")
+  .option("-m, --mkdir <value>", "Create a directory")
+  .option("-t, --touch <value>", "Create a file")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.ls) {
+  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
+  listDirContents(filepath);
+}
