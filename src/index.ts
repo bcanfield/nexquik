39d165a98d9712e9e8f0f86198c074b1593c1a19
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2023-05-10 22:02:50 -0400

    fix: Version increment

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const program = new Command();
 
 console.log(figlet.textSync("Prisnext"));
 program
-  .version("1.0.0")
+  .version(require("../package.json").version)
   .description("An example CLI for managing a directory")
   .option("-l, --ls  [value]", "List directory contents")
   .option("-m, --mkdir <value>", "Create a directory")
