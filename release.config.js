fbdb50eb0a34ef5de33e5d43f080718e1229a718
Brandin Canfield <bcanfield@miletwo.us>
2023-05-10 21:40:27 -0400

    fix: Workflow

--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
         changelogFile: 'CHANGELOG.md'
       }
     ],
-    '@semantic-release/npm',
+    // '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
