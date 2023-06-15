e0a13dbef3b5927a6b4ae5d42402eac5aad94b0c
bcanfield <brandincanfield@gmail.com>
2023-06-15 14:56:41 -0400

    Add prisma to dev env

--- dev/next.config.js
+++ dev/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
