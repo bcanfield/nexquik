94ea11a96caaf8d7c318b020e48abe2b09812ecf
bcanfield <brandincanfield@gmail.com>
2023-08-15 11:20:15 -0400

    Update bin commands

--- src/prismaGenerator.ts
+++ src/prismaGenerator.ts
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
 import { generatorHandler } from "@prisma/generator-helper";
 import { run } from "./cli";
 
+console.log("in generator handler");
 generatorHandler({
   onManifest() {
     return {
