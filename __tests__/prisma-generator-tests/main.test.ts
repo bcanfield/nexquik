89cca6cbede770db5f575faf288b7578b1e9e7de
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2023-08-21 09:07:35 -0400

    fix: Testing and Organization Improvements (#69)
    * Re-organize tests
    
    * Add templateRoot to package json
    
    * Move templateRoot into src directory
    
    * Update template readme

--- __tests__/prisma-generator-tests/main.test.ts
+++ __tests__/prisma-generator-tests/main.test.ts
@@ -1,10 +1,11 @@
 import * as child_process from "child_process";
 import { isDirectoryNotEmpty } from "../utils";
-const testOutputDirectory = "__tests__/prisma-generator/testOutputDirectory";
+const testOutputDirectory =
+  "__tests__/prisma-generator-tests/testOutputDirectory";
 test("base.prisma", async () => {
   child_process.execSync(`rm -rf ${testOutputDirectory}`);
   child_process.execSync(
-    `prisma generate --schema __tests__/prisma-generator/schema.prisma`,
+    `prisma generate --schema __tests__/prisma-generator-tests/schema.prisma`,
     {
       stdio: "inherit",
     }
---
+++ src/templateRoot/README.md
@@ -0,0 +1,25 @@
+This is a [Next.js](https://nextjs.org/) project generated with [`Nexquik`](https://github.com/bcanfield/nexquik).
+
+## Getting Started
+
+Change `DATABASE_URL` in your .env file to point to your desired database.
+```zsh
+DATABASE_URL="YOUR DB HERE"
+```
+
+Install Dependencies
+```zsh
+npm install
+```
+
+Run Prisma Generate
+```bash
+npm run generate
+```
+
+Start the dev server
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
